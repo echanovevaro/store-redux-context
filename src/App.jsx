2513da9a7c77f862d8eb9504de4e595f97d4f3fe
echanovevaro <30557018+echanovevaro@users.noreply.github.com>
2023-10-31 13:38:44 +0100

    empezando carrito de la compra

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,8 @@ import Products from "./components/Products"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import { IS_DEVELOPMENT } from "./config"
-import { useFilters } from "./hooks/useFilters"
-
-// const useFilters = () => {
-//   const [filters, setFilters] = useState({
-//     category: "all",
-//     minPrice: 0,
-//   })
-
-//   const filterProducts = (products) => {
-//     return products.filter((product) => {
-//       return (
-//         product.price >= filters.minPrice &&
-//         (filters.category === "all" || product.category === filters.category)
-//       )
-//     })
-//   }
-//   return { filterProducts, setFilters }
-// }
 
 function App() {
-  // const [products] = useState(initialState)
-
-  // const filterProducts = (products) => {
-  //   return products.filter((product) => {
-  //     return (
-  //       product.price >= filters.minPrice &&
-  //       (filters.category === "all" || product.category === filters.category)
-  //     )
-  //   })
-  // }
-
   return (
     <>
       <Header />
